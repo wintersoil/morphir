4b7fc90ef25c1b482ebfe93291cc16c0493d761c
wintersoil <ali.usmani.182@gmail.com>
2021-02-01 05:27:54 -0500

    commit svg 12
    commit svg 12

--- src/Morphir/Visual/Components/AbstractTreeVisualizer.elm
+++ src/Morphir/Visual/Components/AbstractTreeVisualizer.elm
@@ -6,7 +6,7 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 absString: String
-absString = "AOB -> Add -> [ AVL (Variable) -> F1 , ADB -> Divide -> [ AVL (Variable) -> F2 , AVL (Apply): AVL (Apply): AVL (Reference) -> Morphir.SDK:Basics:multiply __ AVL (Variable) -> F3 __ AVL (Variable) -> F4 ] ]"
+absString = "AOB -> Add -> [ AVL (Variablesdfsdf) -> F1 , ADB -> Dividesdfsdf -> [ AVL (Variable) -> F2 , AVL (Apply): AVL (Apply): AVL (Reference) -> Morphir.SDK:Basics:multiply __ AVL (Variable) -> F3 __ AVL (Variable) -> F4 ] ]"
 
 chopWord : List Int -> Int -> Int -> String -> List String -> List String
 chopWord indices beg end stringy listy=
@@ -20,15 +20,40 @@ createTreeFromString : String -> List (List String)
 createTreeFromString inputString =
   chopWord (String.indexes "," absString) 0 (Maybe.withDefault 0 (List.head (String.indexes "," absString))) inputString []
   |> createFurtherTree []
-  
+
 createFurtherTree: List String -> List String -> List (List String)
 createFurtherTree finalList listy =
   (List.map (\step -> String.split "->" step) listy)
-  
-drawTree: List (List String) -> List (Html msg) -> List (List (Html msg))
+
+maxStringLengthInList: List String -> Int
+maxStringLengthInList listInner =
+  Maybe.withDefault 0 (List.maximum (List.map String.length listInner))
+
+populatePositions: List (List String) -> Int -> Float -> List Float -> List Float
+populatePositions listAll index movingMaximum positions=
+  if index < 0 then
+    positions
+  else if List.member movingMaximum positions then
+   populatePositions listAll (index - 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) positions
+ else
+   populatePositions listAll (index - 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) (positions ++ [movingMaximum])
+
+
+
+previousIndexMax: List (List String) -> Int -> Float -> List Float -> Float
+previousIndexMax listAll index movingMaximum positions=
+  if index < 0 then
+    movingMaximum
+  else if List.member movingMaximum positions then
+   previousIndexMax listAll (index - 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) positions
+ else
+   previousIndexMax listAll (index - 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) (positions ++ [movingMaximum])
+
+drawTree: List (List String) -> List (Html msg)-> List (List (Html msg))
 drawTree listAll htmlAggregation =
   List.indexedMap (\indO elem -> List.indexedMap (\indI elemInner -> div [
                                                         Html.Attributes.style "font-family" "consolas",
+                                                        Html.Attributes.style "z-index" "100",
                                                         Html.Attributes.style "padding" "10px 15px",
                                                         Html.Attributes.style "position" "absolute",
                                                         Html.Attributes.style "background-color" "#333",
@@ -37,7 +62,7 @@ drawTree listAll htmlAggregation =
                                                         Html.Attributes.style "border-bottom" "3px solid rgb(90, 196, 229)",
                                                         Html.Attributes.style "box-shadow" "2px 3px 3px #999, -1px 1px 2px #999",
                                                         Html.Attributes.style "top" ((String.fromInt (400 + ((indO + indI) * 60))) ++ "px"),
-                                                        Html.Attributes.style "left" ((String.fromInt (30 + (indO * 220))) ++ "px")] [Html.text (String.replace "]" "" (String.replace "[" "" elemInner))] ) elem ) listAll
+                                                        Html.Attributes.style "left" ((String.fromFloat (30 + (previousIndexMax listAll (indO-1) 0 positionsX) * 12.3  )) ++ "px")] [Html.text (String.replace "]" "" (String.replace "[" "" elemInner))] ) elem ) listAll
 
 displayTextTree: List (List String) -> List (Html msg)
 displayTextTree listAll=
@@ -54,11 +79,18 @@ displayTextTree listAll=
                         [] [Html.text j]) i))
                       ) listAll)
 
-positionsX: List Int
-positionsX = [30, 100, 200]
+positionsX: List Float
+positionsX = []
 
 main =
-  div [] [(svg
+  let
+    listy =
+      (populatePositions (createTreeFromString absString) (List.length (createTreeFromString absString)) 0 [])
+  in
+  div [] [
+  div [] [div [] (List.map (\p -> div [] p) (drawTree (createTreeFromString absString) []) )],
+  div [] [Html.text (String.join "" ( List.map String.fromFloat (populatePositions (createTreeFromString absString) (List.length (createTreeFromString absString)) 0 [])  ))  ],
+  (svg
     [ Svg.Attributes.width "1200"
     , Svg.Attributes.height "1200"
     , viewBox "0 0 1200 1200"
@@ -68,7 +100,7 @@ main =
     polygon [ fill "#6ec0ff", points "60,425 60,465 65,465 65,425" ] [],
      polygon [ fill "#6ec0ff", points "60,485 60,525 65,525 65,485" ] [],
      polygon [ fill "#6ec0ff", points "60,550 60,590 65,590 65,550" ] [],
-     polygon [ fill "#6ec0ff", points "80,485 250,485 250,480 80,480" ] [],
+     polygon [ fill "#6ec0ff", points ("80,485 " ++ (String.fromFloat ((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 8))  ++ ",485 " ++ (String.fromFloat ((Maybe.withDefault 0.0 (List.head  (List.drop 1 listy) )) * 8)) ++ ",480 80,480") ] [],
 
      polygon [ fill "#6ec0ff", points "280,485 280,525 285,525 285,485" ] [],
      polygon [ fill "#6ec0ff", points "280,550 280,590 285,590 285,550" ] [],
@@ -78,5 +110,5 @@ main =
      polygon [ fill "#6ec0ff", points "496,550 496,590 501,590 501,550" ] [],
      polygon [ fill "#6ec0ff", points "496,610 496,650 501,650 501,610" ] [],
      polygon [ fill "#6ec0ff", points "496,670 496,710 501,710 501,670" ] []
-    ]),
-  div [] [div [] (List.map (\p -> div [] p) (drawTree (createTreeFromString absString) []) )] ]
+    ])
+  ]

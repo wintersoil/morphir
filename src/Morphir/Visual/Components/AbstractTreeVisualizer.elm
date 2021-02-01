20ad1599ab358bd122819a941419ce64483d16b4
wintersoil <ali.usmani.182@gmail.com>
2021-02-01 02:51:25 -0500

    svg commit 8
    svg commit 8

--- src/Morphir/Visual/Components/AbstractTreeVisualizer.elm
+++ src/Morphir/Visual/Components/AbstractTreeVisualizer.elm
@@ -27,7 +27,16 @@ createFurtherTree finalList listy =
   
 drawTree: List (List String) -> List (Html msg) -> List (List (Html msg))
 drawTree listAll htmlAggregation =
-  List.indexedMap (\indO elem -> List.indexedMap (\indI elemInner -> div [Html.Attributes.style "font-family" "consolas", Html.Attributes.style "padding" "10px 20px", Html.Attributes.style "position" "absolute", Html.Attributes.style "top" ((String.fromInt (400 + ((indO + indI) * 60))) ++ "px"), Html.Attributes.style "left" ((String.fromInt (30 + (indO * 180))) ++ "px")] [Html.text elemInner] ) elem ) listAll
+  List.indexedMap (\indO elem -> List.indexedMap (\indI elemInner -> div [
+                                                        Html.Attributes.style "font-family" "consolas",
+                                                        Html.Attributes.style "padding" "10px 15px",
+                                                        Html.Attributes.style "position" "absolute",
+                                                        Html.Attributes.style "background-color" "#333",
+                                                        Html.Attributes.style "color" "rgb(96, 202, 245)",
+                                                        Html.Attributes.style "border" "3px solid rgb(116, 222, 255)",
+                                                        Html.Attributes.style "box-shadow" "2px 3px 3px #999, -1px 1px 2px #999",
+                                                        Html.Attributes.style "top" ((String.fromInt (400 + ((indO + indI) * 60))) ++ "px"),
+                                                        Html.Attributes.style "left" ((String.fromInt (30 + (indO * 220))) ++ "px")] [Html.text elemInner] ) elem ) listAll
 
 displayTextTree: List (List String) -> List (Html msg)
 displayTextTree listAll=
@@ -54,9 +63,15 @@ main =
     , viewBox "0 0 1200 1200"
     ,Html.Attributes.style "position" "absolute"
     ]
-    [
-    polygon [ fill "#6ec0ff", points "70,425 70,465 75,465 75,425" ] [],
-     polygon [ fill "#6ec0ff", points "70,485 70,525 75,525 75,485" ] [],
-     polygon [ fill "#6ec0ff", points "70,550 70,590 75,590 75,550" ] []
+    [ 
+    polygon [ fill "#6ec0ff", points "60,425 60,465 65,465 65,425" ] [],
+     polygon [ fill "#6ec0ff", points "60,485 60,525 65,525 65,485" ] [],
+     polygon [ fill "#6ec0ff", points "60,550 60,590 65,590 65,550" ] [],
+     polygon [ fill "#6ec0ff", points "80,485 250,485 250,480 80,480" ] [],
+     
+     polygon [ fill "#6ec0ff", points "280,485 280,525 285,525 285,485" ] [],
+     polygon [ fill "#6ec0ff", points "280,550 280,590 285,590 285,550" ] [],
+     polygon [ fill "#6ec0ff", points "280,610 280,650 285,650 285,610" ] [],
+     polygon [ fill "#6ec0ff", points "285,550 480,550 480,545 285,545" ] []
     ]),
-  div [] [div [] (displayTextTree (createTreeFromString absString)), div [] (List.map (\p -> div [] p) (drawTree (createTreeFromString absString) []) )] ]
+  div [] [div [] (List.map (\p -> div [] p) (drawTree (createTreeFromString absString) []) )] ]

78e6c486b7565a3758e443a58faa9b26cd88f04c
wintersoil <ali.usmani.182@gmail.com>
2021-02-01 06:50:51 -0500

    svg commit 14
    svg commit 14

--- src/Morphir/Visual/Components/AbstractTreeVisualizer.elm
+++ src/Morphir/Visual/Components/AbstractTreeVisualizer.elm
@@ -6,7 +6,7 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 absString: String
-absString = "AOB -> Add -> [ AVL (Variablesdfsdf) -> F1 , ADB -> Dividesdfsdf -> [ AVL (Variable) -> F2 , AVL (Apply): AVL (Apply): AVL (Reference) -> Morphir.SDK:Basics:multiply __ AVL (Variable) -> F3 __ AVL (Variable) -> F4 ] ]"
+absString = "AOB -> Add -> [ AVL (Variables) -> F1 , ADB -> Divides-> [ AVL (Variable) -> F2 , AVL (Apply): AVL (Apply): AVL (Reference) -> Morphir.SDK:Basics:multiply __ AVL (Variable) -> F3 __ AVL (Variable) -> F4 ] ]"
 
 chopWord : List Int -> Int -> Int -> String -> List String -> List String
 chopWord indices beg end stringy listy=
@@ -31,12 +31,12 @@ maxStringLengthInList listInner =
 
 populatePositions: List (List String) -> Int -> Float -> List Float -> List Float
 populatePositions listAll index movingMaximum positions=
-  if index < 0 then
+  if index == (List.length listAll) then
     positions
   else if List.member movingMaximum positions then
-   populatePositions listAll (index - 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) positions
+   populatePositions listAll (index + 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) positions
  else
-   populatePositions listAll (index - 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) (positions ++ [movingMaximum])
+   populatePositions listAll (index + 1) ( movingMaximum + (toFloat (Maybe.withDefault 0 (List.maximum (List.indexedMap (\ind elem -> if ind == index then (maxStringLengthInList elem) else 0) listAll)))) ) (positions ++ [movingMaximum])
 
 
 
@@ -62,7 +62,7 @@ drawTree listAll htmlAggregation =
                                                         Html.Attributes.style "border-bottom" "3px solid rgb(90, 196, 229)",
                                                         Html.Attributes.style "box-shadow" "2px 3px 3px #999, -1px 1px 2px #999",
                                                         Html.Attributes.style "top" ((String.fromInt (400 + ((indO + indI) * 60))) ++ "px"),
-                                                        Html.Attributes.style "left" ((String.fromFloat (30 + (previousIndexMax listAll (indO-1) 0 positionsX) * 12.3  )) ++ "px")] [Html.text (String.replace "]" "" (String.replace "[" "" (elemInner ++ ((String.fromFloat (30 + (previousIndexMax listAll (indO-1) 0 positionsX) * 12.3  )) ++ "px")) ))] ) elem ) listAll
+                                                        Html.Attributes.style "left" ((String.fromFloat (30 + (previousIndexMax listAll (indO-1) 0 positionsX) * 12.3  )) ++ "px")] [Html.text (String.replace "]" "" (String.replace "[" "" (elemInner) ))] ) elem ) listAll
 
 displayTextTree: List (List String) -> List (Html msg)
 displayTextTree listAll=
@@ -85,11 +85,11 @@ positionsX = []
 main =
   let
     listy =
-      (populatePositions (createTreeFromString absString) (List.length (createTreeFromString absString)) 0 [])
+      (populatePositions (createTreeFromString absString) (0) 0 [])
   in
   div [] [
   div [] [div [] (List.map (\p -> div [] p) (drawTree (createTreeFromString absString) []) )],
-  div [] [Html.text (String.join "   " ( List.map String.fromFloat (populatePositions (createTreeFromString absString) (List.length (createTreeFromString absString)) 0 [])  ))  ],
+  div [] [Html.text (String.join "   " ( List.map String.fromFloat (listy)  ))  ],
   (svg
     [ Svg.Attributes.width "1200"
     , Svg.Attributes.height "1200"
@@ -100,12 +100,15 @@ main =
     polygon [ fill "#6ec0ff", points "60,425 60,465 65,465 65,425" ] [],
      polygon [ fill "#6ec0ff", points "60,485 60,525 65,525 65,485" ] [],
      polygon [ fill "#6ec0ff", points "60,550 60,590 65,590 65,550" ] [],
-     polygon [ fill "#6ec0ff", points ("80,485 " ++ (String.fromFloat ((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 8))  ++ ",485 " ++ (String.fromFloat ((Maybe.withDefault 0.0 (List.head  (List.drop 1 listy) )) * 8)) ++ ",480 80,480") ] [],
+     polygon [ fill "#6ec0ff", points ("80,485 " ++ (String.fromFloat ((Maybe.withDefault 0.0 (List.head (List.drop 0 listy) )) * 8))  ++ ",485 " ++ (String.fromFloat ((Maybe.withDefault 0.0 (List.head  (List.drop 0 listy) )) * 8)) ++ ",480 80,480") ] [],
 
-     polygon [ fill "#6ec0ff", points "280,485 280,525 285,525 285,485" ] [],
-     polygon [ fill "#6ec0ff", points "280,550 280,590 285,590 285,550" ] [],
-     polygon [ fill "#6ec0ff", points "280,610 280,650 285,650 285,610" ] [],
-     polygon [ fill "#6ec0ff", points "285,550 480,550 480,545 285,545" ] [],
+     polygon [ fill "#6ec0ff", points ("" ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55))  ++ ",485 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55)) ++ ",525 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 50)) ++ ",525 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 50)) ++  ",485") ] [],
+
+     polygon [ fill "#6ec0ff", points ("" ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55))  ++ ",550 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55)) ++ ",590 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 50)) ++ ",590 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 50)) ++  ",550") ] [],
+
+     polygon [ fill "#6ec0ff", points ("" ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55))  ++ ",610 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55)) ++ ",650 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 50)) ++ ",650 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 50)) ++  ",610") ] [],
+
+     polygon [ fill "#6ec0ff", points ( (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55)) ++ ",550 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 2 listy) )) * 12.3) + 55))  ++ ",550 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head  (List.drop 2 listy) )) * 12.3) + 55)) ++ ",545 " ++ (String.fromFloat (((Maybe.withDefault 0.0 (List.head (List.drop 1 listy) )) * 12.3) + 55)) ++ ",545") ] [],
 
      polygon [ fill "#6ec0ff", points "496,550 496,590 501,590 501,550" ] [],
      polygon [ fill "#6ec0ff", points "496,610 496,650 501,650 501,610" ] [],
